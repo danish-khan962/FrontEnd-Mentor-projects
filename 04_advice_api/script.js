fffdff6cfe2f26f9878fbadd06ef50a47dd70a89
Danish Khan <dk831675@gmail.com>
2025-01-08 15:56:22 +0530

    default advice on landing

--- 04_advice_api/script.js
+++ 04_advice_api/script.js
@@ -2,7 +2,7 @@ const dice = document.querySelector('.dice-div');
 let adviceCard = document.querySelector('.adviceCard');
 
 dice.addEventListener("click",()=>{
-    const url = " 	https://api.adviceslip.com/advice";
+    const url = "https://api.adviceslip.com/advice";
     fetch(url)
     .then(response => response.json())
     .then(data=>{
@@ -21,3 +21,12 @@ dice.addEventListener("click",()=>{
         `
     })
 })
+
+/*Default advice*/
+window.onload = () => {
+    adviceCard.innerHTML = `
+        <p class="advice_number">ADVICE #117</p>
+        <p class="advice">“It is easy to sit up and take notice, what's difficult is getting up and taking action.”</p>
+        <img src="./images/pattern-divider-desktop.svg" alt="pattern-divider.svg" class="pattern-divider">
+    `;
+};
