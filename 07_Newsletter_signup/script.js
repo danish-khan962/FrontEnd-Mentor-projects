369cf3a0185389f35a44f97812baca958fcc1e05
Danish Khan <dk831675@gmail.com>
2025-01-10 14:11:40 +0530

    minor-chanes

--- 07_Newsletter_signup/script.js
+++ 07_Newsletter_signup/script.js
@@ -9,8 +9,10 @@ button.addEventListener("click", function(event) {
     let email = emailInput.value;
     let errorElement = document.querySelector('.error');
     let emailSpan = document.querySelector('.em');
+    let dismissBtn = document.querySelector('.dismiss');
+    let browseButton = document.querySelector('.browse');
     
-    if (email.indexOf('@') === -1) {
+    if ((email.indexOf('@') === -1) || email.length<=8) {
         emailInput.style.color = '#FF6A3A';
         emailInput.style.border = '2px solid #FF6A3A';
         emailInput.style.backgroundColor = 'rgba(255, 159, 122, 0.1)';
@@ -23,6 +25,11 @@ button.addEventListener("click", function(event) {
         document.querySelector('.successCard').style.display = 'flex';
         emailSpan.innerHTML = `${email}`;
         console.log("Email: " + email);
+
+        dismissBtn.addEventListener("click",()=>{
+            document.querySelector('.successCard').style.display = 'none';
+            document.querySelector('.browse').style.display = 'flex';
+        })
     }
 
     return returnval; 
